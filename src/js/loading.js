4429d33f5f5de281a5b79aa08befc36893aefcee
Dm <orda17@mail.ru>
2021-07-19 19:41:14 +0300

    step 1

--- src/js/loading.js
+++ src/js/loading.js
@@ -11,4 +11,18 @@
   const canvasTitle = localStorage.getItem("md-canvasTitle");
   svgCanvas.setAttribute("title", canvasTitle ? "Loading " + canvasTitle  : "Loading Drawing");
   const svg = workarea.querySelector("svg");
+  function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+//file:///blah-blah.../index.html?test=1&testy=2
+  //console.log(`---------------------------\n`,findGetParameter('test'));
+  console.log(`---------------------------\n`,findGetParameter('testy'));
+  localStorage.setItem('test', findGetParameter('test'));
 })();
